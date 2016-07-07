93d94546e1227ec695d8adeb1440281c6f6db084
fedehipper <federicohipper@gmail.com>
2016-07-07 20:21:55 -0300

    refactor

--- src/tadQuest/Inventario.scala
+++ src/tadQuest/Inventario.scala
@@ -10,8 +10,7 @@ case class Inventario(items: List[Item] = Nil) {
   def equipar(heroe: Heroe, item: Item): Try[Inventario] = Try(
     if(item cumpleCondicion heroe) {
       val equipamientoDe = item.sector match {    
-        case ArmaSimple => equiparArmaSimple _
-        case ArmaDoble => equiparArmaDoble _
+        case arma if(List(ArmaSimple, ArmaDoble).contains(arma)) => equiparArma _
         case _ => equiparSimple _
       }
       equipamientoDe(item)
@@ -19,23 +18,28 @@ case class Inventario(items: List[Item] = Nil) {
     else throw NoSePudoEquiparUnItem
   )
   
+  def agregarItem(item: Item) = copy(item :: items)
+  
   def equiparSimple(item: Item) = item.sector match{
-    case Talisman => copy(item :: items)
-    case _ => copy(item :: items.filterNot( _ == item.sector))
+    case Talisman => agregarItem(item)
+    case _ => copy(item :: items.filterNot(_ == item.sector))
   }
   
-  def equiparArmaDoble(item: Item) = copy(item :: items.filterNot(i => i.sector == ArmaSimple || i.sector == ArmaDoble))
-  
-  def equiparArmaSimple(item: Item) = {
-    val armasSimples = items.filter(_.sector == ArmaSimple)
-    if (items.exists(_.sector == ArmaDoble)) copy(item:: items.filterNot(_.sector == ArmaDoble))
-    else {
-      if (armasSimples.size < 2) copy(item :: items)
-      else copy(item :: armasSimples.head :: items.filterNot(_.sector == ArmaSimple))
+  def equiparArma(item: Item) = {
+    item.sector match {
+      case ArmaSimple => {
+        val armasSimples = items.filter(_.sector == ArmaSimple)
+        if (items.exists(_.sector == ArmaDoble)) copy(item:: items.filterNot(_.sector == ArmaDoble))
+        else {
+          if (armasSimples.size < 2) agregarItem(item)
+          else copy(item :: armasSimples.head :: items.filterNot(_.sector == ArmaSimple))
+        }
+      }
+      case ArmaDoble => 
+        copy(item :: items.filterNot(i => i.sector == ArmaSimple || i.sector == ArmaDoble))
     }
   }
   
-
   def desequipar(item: Item) = copy(items.filterNot(_ == item))
   
   def valorDeItems(i: (Item, Heroe, Double) => Double)(heroe: Heroe, valor: Double) = {
